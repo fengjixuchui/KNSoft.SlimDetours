07db2ecf70a3f600907b5d792e3123b0e6e53acb
Ratin Gao <ratin@knsoft.org>
2025-02-04 18:39:30 +0800

    [TEST][SYNC] Sync MSVC template and add unit test "Instruction" for testing various instructions in different platforms

---
+++ Source/Demo/Instruction.c
@@ -0,0 +1,61 @@
+﻿#include "Demo.h"
+#include "Instruction.inl"
+
+typedef
+_Function_class_(FN_INSTRUCTION)
+ULONG_PTR
+_cdecl
+FN_INSTRUCTION(VOID);
+
+#if defined(_M_X64)
+
+EXTERN_C FN_INSTRUCTION* SimpleInstructionFunc1X64Ptr;
+
+#elif defined(_M_IX86)
+
+EXTERN_C FN_INSTRUCTION* SimpleInstructionFunc1X86Ptr;
+
+#endif
+
+static
+_Function_class_(FN_INSTRUCTION)
+ULONG_PTR
+_cdecl
+Hooked_InstructionFunc(VOID)
+{
+    return (ULONG_PTR)PRESET_RETURN_VALUE * 2;
+}
+
+static FN_INSTRUCTION** g_apfnInstructionFunctions[] = {
+#if defined(_M_X64)
+    &SimpleInstructionFunc1X64Ptr,
+#elif defined(_M_IX86)
+    &SimpleInstructionFunc1X86Ptr,
+#endif
+    NULL
+};
+
+TEST_FUNC(Instruction)
+{
+    ULONG i;
+    HRESULT hr;
+    FN_INSTRUCTION *pfn1, *pfn2;
+
+    for (i = 0; i < ARRAYSIZE(g_apfnInstructionFunctions) && g_apfnInstructionFunctions[i] != NULL; i++)
+    {
+        pfn1 = *g_apfnInstructionFunctions[i];
+        pfn2 = pfn1;
+        if (pfn1() != (ULONG_PTR)PRESET_RETURN_VALUE)
+        {
+            TEST_SKIP("Instruction Function #%lu did not return the preset value\n", i);
+            continue;
+        }
+        hr = SlimDetoursInlineHook(TRUE, (PVOID*)&pfn1, Hooked_InstructionFunc);
+        TEST_OK(SUCCEEDED(hr));
+        if (SUCCEEDED(hr))
+        {
+            TEST_OK(pfn1() == (ULONG_PTR)PRESET_RETURN_VALUE);
+            TEST_OK(pfn2() == (ULONG_PTR)PRESET_RETURN_VALUE * 2);
+        }
+    }
+}
